cb984a83f4f8423c17dc1a968d7eb2516508256e
Dmytro <dima.net.dd@gmail.com>
2023-02-16 15:29:55 +0200

    add slider

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,13 @@
 import * as webpFuncs from './modules/webpFunctions.js';
+
 webpFuncs.isWebp();
+
+$(document).ready(function () {
+  $('.media-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+  });
+  $('.slick-prev').html('<img src="img/arrow-left.svg" />');
+  $('.slick-next').html('<img src="img/arrow-right.svg" />');
+});
